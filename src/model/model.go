76e27ec7e1d939b179518b9f5863441e6499c597
Emilia <emilia.ferlin@sbsistemas.com.br>
2026-04-08 11:18:53 -0300

    refactor: ajusta model para incluir novas classes

--- src/model/model.go
+++ src/model/model.go
@@ -3,7 +3,27 @@ package model
 type Perfil struct {
 	ID         int    `json:"id"`
 	Nome       string `json:"nome"`
+	Idade      int    `json:"idade"`
 	Disciplina string `json:"disciplina"`
-	Nivel      string `json:"nivel"`
-	Estilo     string `json:"estilo"`
+	Nivel      string `json:"nivel"`  //sem conhecimento - conhecimento médio - conhecimento alto
+	Estilo     string `json:"estilo"` // mais silencioso - gosta de argumentar
+}
+
+type Sessao struct {
+	ID             int    `json:"id"`
+	Titulo         string `json:"titulo"`
+	Disciplina     string `json:"disciplina"`
+	Nivel          string `json:"nivel"`  //sem conhecimento - conhecimento médio - conhecimento alto
+	Estilo         string `json:"estilo"` // mais silencioso - gosta de argumentar
+	DataHoraInicio string `json:"dataHoraInicio"`
+	DuracaoMinutos int    `json:"duracaoMinutos"`
+	Vagas          int    `json:"vagas"`
+}
+
+type Match struct {
+	ID       int  `json:"id"`
+	PerfilID int  `json:"perfilId"`
+	SessaoID int  `json:"sessaoId"`
+	Score    int  `json:"score"`
+	Aceito   bool `json:"aceito"` // usuário aceitou ou não
 }
